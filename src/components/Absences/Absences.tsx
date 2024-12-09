e189c9045306921bf8e59bc575179c514aacff85
djmahony <danmahony85@gmail.com>
2024-12-09 08:44:45 +0000

    Get conflicts for absences

--- src/components/Absences/Absences.tsx
+++ src/components/Absences/Absences.tsx
@@ -5,31 +5,52 @@ import { Absence } from '../../types/absenceTypes';
 
 const getAbsences = async () => {
     try {
-
         const response = await fetch('https://front-end-kata.brighthr.workers.dev/api/absences');
-
-        if(!response.ok) {
+        if (!response.ok) {
             throw new Error(`Error! Status: ${response.status}`);
         }
-
         const data = await response.json();
-        
         return data;
-    } catch(err) {
-        console.log("Error fetching data:", err);
+    } catch (err) {
+        console.log("Error fetching absences:", err);
         return [];
     }
 }
 
-const Absences = () => {
+const getConflicts = async (id: number) => {
+    try {
+        const url = `https://front-end-kata.brighthr.workers.dev/api/conflict/${id}`;
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        return data; 
+    } catch (err) {
+        console.log("Error fetching conflicts:", err);
+        return { conflicts: false }; 
+    }
+}
 
+const Absences = () => {
     const [absences, setAbsences] = useState<Absence[]>([]);
-    const [loading, setLoading] = useState<boolean>(true)
+    const [loading, setLoading] = useState<boolean>(true);
 
     useEffect(() => {
         const fetchAbsences = async () => {
             const data = await getAbsences();
-            setAbsences(data);
+            
+            const updatedAbsences = await Promise.all(
+                data.map(async (absence: Absence) => {
+                    const conflict = await getConflicts(absence.id);
+                    return {
+                        ...absence,
+                        conflicts: conflict.conflicts, 
+                    };
+                })
+            );
+
+            setAbsences(updatedAbsences);
             setLoading(false);
         };
 
@@ -39,9 +60,9 @@ const Absences = () => {
     return (
         <section className={styles.absences}>
             <h1>Absences</h1>
-            {loading ? <p>Loading</p> : <AbsenceTable absences={absences} />}
+            {loading ? <p>Loading...</p> : <AbsenceTable absences={absences} />}
         </section>
-    )
+    );
 }
 
 export default Absences;
