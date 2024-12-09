ab01602a2c361a2e84ae3f5aa8e414328c822658
djmahony <danmahony85@gmail.com>
2024-12-09 08:43:34 +0000

    Add absence display details

--- src/components/AbsenceDisplay/AbsenceDisplay.tsx
+++ src/components/AbsenceDisplay/AbsenceDisplay.tsx
@@ -1,5 +1,24 @@
-const absence = () => {
+import styles from './AbsenceDisplay.module.scss';
+import { Absence } from '../../types/absenceTypes';
+import React from 'react';
 
+interface AbsenceDisplayProps {
+    absence: Absence;
 }
 
-export default absence;
+const AbsenceDisplay: React.FC<AbsenceDisplayProps> = ({absence}) => {
+    return (
+        <tr>
+            <td>{absence.id}</td>
+            <td>{absence.startDate}</td>
+            <td>{absence.days}</td>
+            <td>{absence.absenceType}</td>
+            <td>{absence.employee.firstName}</td>
+            <td>{absence.employee.lastName}</td>
+            <td>{absence.approved ? 'yes' : 'no'}</td>
+            <td>{absence.conflicts ? 'yes' : 'no'}</td>
+        </tr>
+    )
+}
+
+export default AbsenceDisplay;
